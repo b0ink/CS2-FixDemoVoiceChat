1d842431f4a534da92b93284fdc9ffa1df52f86d
b0ink <40929320+b0ink@users.noreply.github.com>
2024-01-15 20:37:25 +1100

    Initial Commit

---
+++ FixDemoVoiceChat.cs
@@ -0,0 +1,36 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+
+namespace FixDemoVoiceChat;
+public class FixDemoVoiceChat : BasePlugin
+{
+    public override string ModuleName => "Fix Voice Chat in Demo Recordings";
+    public override string ModuleAuthor => "BOINK";
+    public override string ModuleVersion => "1.0.0";
+
+    public override void Load(bool hotReload)
+    {
+        RegisterEventHandler<EventRoundStart>(((@event, info) =>
+        {
+            Server.NextFrame(() =>
+            {
+                FixHltvVoiceChat();
+            });
+            return HookResult.Continue;
+        }));
+    }
+
+    void FixHltvVoiceChat()
+    {
+        var HLTV = Utilities.GetPlayers().Where(p => p.IsHLTV).First();
+
+        if (HLTV != null && HLTV.IsValid)
+        {
+            HLTV.VoiceFlags &= ~VoiceFlags.Normal;
+            HLTV.VoiceFlags |= VoiceFlags.All;
+            HLTV.VoiceFlags |= VoiceFlags.ListenAll;
+        }
+    }
+
+}
+
